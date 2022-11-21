fd980ffc13a7fccfd7ba373a1f977866ba90decf
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-11-21 21:51:49 +0100

    Return type annotations

--- functions/functions.ts
+++ functions/functions.ts
@@ -3,3 +3,23 @@ function multiply(num: number) {
 }
 
 multiply(1);
+
+function sayHello(name: string[]): string {
+  return `Hello ${name.map(el => el + ',').join(' ')}`;
+}
+
+console.log(sayHello(['Peter', 'Thomas']));
+
+function abc(num: number = 1): number {
+  return num;
+}
+
+abc(1);
+
+const arrowTest = (n: number): number => {
+  return n ** n;
+};
+
+const secondArrowTest = (str: string): string => str;
+
+const random = (str: string) => (Math.random() < 0.5 ? str : +str);
