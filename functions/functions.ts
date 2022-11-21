6d6c954b69872f27d1edfaf16ee61bb4e8cb5bc7
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-11-21 22:16:27 +0100

    functions

--- functions/functions.ts
+++ functions/functions.ts
@@ -23,3 +23,28 @@ const arrowTest = (n: number): number => {
 const secondArrowTest = (str: string): string => str;
 
 const random = (str: string) => (Math.random() < 0.5 ? str : +str);
+
+const testArr = [1, 2, 3, 4, 5];
+
+testArr.reduce((acc, cur) => acc + cur);
+
+const voidTest = (): void => {
+  console.log('Hello World!');
+  //   return 'Hello World!'; Void = no return
+};
+
+const testFunction = () => {
+  return 1 * 1;
+};
+
+const testFunctionTwo = () => {
+  1 * 1;
+};
+
+const neverTest = (): never => {
+  while (true) {}
+};
+
+const neverTestTwo = (): never => {
+  throw new Error('Test');
+};
