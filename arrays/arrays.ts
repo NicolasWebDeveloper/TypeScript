75fda34ec91726c4a713f3f73981dc1ed29d87da
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-11-25 18:13:01 +0100

    Learned abou Arrays

---
+++ arrays/arrays.ts
@@ -0,0 +1,17 @@
+const users: string[] = ['Peter', 'Jonas'];
+const numbers: number[] = [1, 2, 3, 4, 5];
+const emptyArray: [] = [];
+const names: Array<String> = ['a', 'b', 'c', 'd', 'e'];
+
+type user = {
+  name: string;
+  pw: string;
+};
+
+const userList: user[] = [{ name: 'Peter', pw: 'test' }];
+
+const tictactoe: string[][] = [
+  ['X', 'O', 'X'],
+  ['O', 'X', 'O'],
+  ['X', 'X', 'O'],
+];
